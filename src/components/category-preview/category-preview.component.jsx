41c92eac3d4812356888241edb5111dc1fbe4575
syedkhalidashraf <i.khalidashraf@gmail.com>
2022-07-21 20:01:53 -0400

    .

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,6 +1,7 @@
 import "./category-preview.styles.scss";
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 import ProductCard from "../product-card/product-card";
 
@@ -8,7 +9,9 @@ const CategoryPreview = ({ title, products }) => {
 	return (
 		<div className='category-preview-container'>
 			<h2>
-				<span className='title'>{title.toUpperCase()}</span>
+				<Link className='title' to={title}>
+					{title.toUpperCase()}
+				</Link>
 			</h2>
 			<div className='preview'>
 				{products
